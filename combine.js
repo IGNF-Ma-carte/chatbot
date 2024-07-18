61d42aa26bbdaecc36fcd784a68479c68deb5026
viglino <jean-marc.viglino@ign.fr>
2024-07-18 08:47:20 +0200

    [UPD]

--- combine.js
+++ combine.js
@@ -8,18 +8,32 @@ const mainmd = 'main.md'
 // Destination file
 const destination = './chat.txt'
 
-function nextFile(files, directory) {
+const colorStart = '\x1b[36m\x1b[1m\x1b[44m';
+const colorEnd = '\x1b[0m';
+
+console.log (colorStart, '😼 ChatMD', colorEnd)
+console.log (colorStart, ' Un chatbot personnalisé à partir de fichiers en Markdown', colorEnd, '\n')
+
+function nextFile(files, directory, options) {
   const file = files.shift();
   if (!file) {
+    if (directory === root) {
+      console.log(colorStart, 
+        options.nb + ' files (' + options.nbdir + ' directories) ', 
+        colorEnd
+      )
+    }
     //finish
     return
   }
   const isDir = fs.lstatSync(directory + '/' + file).isDirectory() 
   if (isDir) {
+    options.nbdir++;
     // Process dir
     const files2 = fs.readdirSync(directory + '/' + file);
-    nextFile(files2, directory + '/' + file)
+    nextFile(files2, directory + '/' + file, options)
   } else {
+    options.nb++;
     // Process file
     let content = fs.readFileSync(directory + '/' + file, { encoding: 'utf8' });
     // Process local ref [](./ref) or [](../ref)
@@ -43,9 +57,9 @@ function nextFile(files, directory) {
     }
     // Write file
     fs.appendFileSync(destination, content + '\n\n')
-    console.log(directory + '/' + file)
+    // console.log(directory + '/' + file)
   }
-  nextFile(files, directory);
+  nextFile(files, directory, options);
 }
 
 function combine() {
@@ -58,7 +72,7 @@ function combine() {
   files.unshift(mainmd)
   // Clear file
   fs.writeFileSync(destination, '');
-  nextFile(files, root)
+  nextFile(files, root, { nb:0, nbdir:0 })
 }
 
 // Start
